e1e75a9017b1a19b855fbe600105cc82424e9645
Shahriar Kamal <shahriar2203041@gmail.com>
2025-06-27 16:44:55 +0600

    Create seven_segment_display_H.ino

---
+++ Arduino_Basics/seven_segment_display_H.ino
@@ -0,0 +1,19 @@
+void setup() {
+  pinMode(9, OUTPUT);   // a
+  pinMode(10, OUTPUT);  // b
+  pinMode(11, OUTPUT);  // c
+  pinMode(5, OUTPUT);   // d
+  pinMode(3, OUTPUT);   // e
+  pinMode(6, OUTPUT);   // f
+  pinMode(4, OUTPUT);   // g
+}
+
+void loop() {
+  digitalWrite(9, LOW);    // a OFF
+  digitalWrite(10, HIGH);  // b ON
+  digitalWrite(11, HIGH);  // c ON
+  digitalWrite(5, HIGH);    // d OFF
+  digitalWrite(3, LOW);   // e ON
+  digitalWrite(6, HIGH);   // f ON
+  digitalWrite(4, HIGH);   // g ON
+}
