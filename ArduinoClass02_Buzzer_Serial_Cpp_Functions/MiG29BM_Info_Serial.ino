c73e0c8d07b7a08faab1858f6efeb19a71f6abe7
Shahriar Kamal <shahriar2203041@gmail.com>
2025-06-29 22:01:54 +0600

    Create MiG29BM_Info_Serial.ino

---
+++ ArduinoClass02_Buzzer_Serial_Cpp_Functions/MiG29BM_Info_Serial.ino
@@ -0,0 +1,16 @@
+void setup() {
+  Serial.begin(9600);
+  
+  Serial.println("---- Bangladesh Aircraft Info ----");
+  Serial.println("Name: Mikoyan MiG‑29 (MiG‑29BM)");
+  Serial.println("Type: 4th‑generation Multirole Fighter");
+  Serial.println("Operator: Bangladesh Air Force");
+  Serial.println("In service: 8 units (upgraded via Belarus)");
+  Serial.println("Top Speed: Mach 2.25 (~2,400 km/h)");
+  Serial.println("Role: Air superiority & ground attack");
+  Serial.println("-------------------------------");
+}
+
+void loop() {
+  // No repeat needed
+}
