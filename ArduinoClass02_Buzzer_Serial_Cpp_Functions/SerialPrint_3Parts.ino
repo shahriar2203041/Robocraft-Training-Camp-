612ccb1bb2cc3e79098d84a0d1ae2e97aabaca39
Shahriar Kamal <shahriar2203041@gmail.com>
2025-06-29 20:33:33 +0600

    Create SerialPrint_3Parts.ino

---
+++ ArduinoClass02_Buzzer_Serial_Cpp_Functions/SerialPrint_3Parts.ino
@@ -0,0 +1,10 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("Hello World! ");
+  Serial.print("I am Shahriar Kamal. ");
+  Serial.println("I am learning Arduino.");
+  delay(1000); // Wait 1 second
+}
