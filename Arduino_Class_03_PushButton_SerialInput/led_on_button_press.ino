9cb8edbf7d925d64f2a06786b153378ed2f93f4f
Shahriar Kamal <shahriar2203041@gmail.com>
2025-07-03 20:04:32 +0600

    Create led_on_button_press.ino

---
+++ Arduino_Class_03_PushButton_SerialInput/led_on_button_press.ino
@@ -0,0 +1,19 @@
+int led = 8;
+int push = 7;
+int state;
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(push, INPUT_PULLUP);
+}
+
+void loop() {
+  state = digitalRead(push);
+
+  if (state == 1) {
+    digitalWrite(led,HIGH);
+
+
+  } else if (state == 0) {
+    digitalWrite(led,LOW);
+  }
+}
